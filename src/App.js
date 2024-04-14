4e64746344d8327160cb0a1dd5586367223d17e3
Oliver Gao <ogao@umich.edu>
2024-04-14 13:26:20 -0400

    Revert "UI"
    This reverts commit 9a3fe1ce48a0001ce24a47ec20c73c4bc3b3fc14.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { testRes } from './res';
 
 const PlaylistReference = ({ playlists, isPlaylistsLoading }) => {
   return (
-    <div className={`p-2 mb-8 max-h-full overflow-y-auto ${isPlaylistsLoading ? 'opacity-0' : 'opacity-100 transition-opacity duration-500 ease-in-out'}`}>
+    <div className="p-2 mb-8 max-h-full overflow-y-auto ">
       <h2 className="text-lg font-bold mb-4">Playlist Reference</h2>
       {isPlaylistsLoading ? (
         <p>Loading playlists...</p>
@@ -35,23 +35,23 @@ const PlaylistReference = ({ playlists, isPlaylistsLoading }) => {
 
 
 const Response = ({ promptResponse, isPromptLoading, recv }) => (
-  <div className={`mb-8 ${!recv && !isPromptLoading ? 'opacity-0' : 'opacity-100 transition-opacity duration-500 ease-in-out'}`}>
+  <div className="mb-8">
     <h2 className="text-lg font-bold mb-4 border-b border-gray-300 pt-4 pb-1 px-6">Response</h2>
     <div className="overflow-y-auto max-h-[450px]">
-      {isPromptLoading || !recv ? (
+      {isPromptLoading || !recv ?  (
         isPromptLoading ? <p className='px-6'>Loading...</p>
-          : <p className='px-6'>Please enter a prompt to receive a response.</p>
+        : <p className='px-6'>Please enter a prompt to receive a response.</p>
       ) : (
         <div className='px-6'>
           <h3 className='text-lg font-bold mb-4 border-b bg-red-200'>Summary</h3>
           <p><Markdown>{promptResponse.summary}</Markdown></p>
-
+          
           <h3 className='text-lg font-bold my-4 border-b bg-red-200'>Key Moments</h3>
           {
             Object.entries(promptResponse.timestamps).map(([video_id, items], index) => (
               <div key={index} className="mb-2">
                 <h3 className="text-lg font-bold">Video {index + 1}</h3>
-
+    
                 {items.map((item, index) => (
                   <div key={index}>
                     <a className='text-blue-600 underline mr-2' href={item.video_url} target='_blank' rel="noreferrer">{item.Timestamp}</a>
@@ -82,9 +82,9 @@ const TitleCard = ({ handleSubmit }) => (
       <button
         type="submit"
         className="bg-gray-100 text-gray-500 py-2 px-2 mb-2 rounded-md hover:bg-gray-200 focus:outline-none focus:bg-gray-200 transition-all duration-300 ease-in-out border border-gray-300"
-        style={{ paddingTop: "5px", paddingBottom: "5px", marginLeft: "4px" }}
+        style={{ paddingTop: "5px", paddingBottom: "5px", marginLeft: "4px" }} 
       >
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-3 transform rotate-180" viewBox="0 0 20 20" fill="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-3 transform rotate-180" viewBox="0 0 20 20" fill="currentColor"> 
           <path fillRule="evenodd" d="M10 3a1 1 0 011 1v11.586l3.293-3.293a1 1 0 111.414 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 111.414-1.414L9 15.586V4a1 1 0 011-1z" clipRule="evenodd" />
         </svg>
       </button>
@@ -116,14 +116,14 @@ export default function App() {
         .then(data => {
           console.log(data);
           setPlaylists(data);
-
+          
           setIsPlaylistsLoading(false);
         })
         .catch(error => {
           console.error('Error:', error.message)
           setIsPlaylistsLoading(false);
-        });
-
+      });
+      
     } catch (error) {
       console.error('Error:', error.message);
       setIsPlaylistsLoading(false);
@@ -146,7 +146,7 @@ export default function App() {
         .catch(error => {
           console.error('Error:', error.message)
           setIsPromptLoading(false);
-        });
+      });
 
     } catch (error) {
       console.error('Error:', error.message);
@@ -164,28 +164,52 @@ export default function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center">
       <div className="max-w-screen-xl mx-auto mt-8 mb-10 flex flex-col justify-center items-center">
-        {/* Title/Prompt Card */}
-        <div className="bg-red-200">
-          <TitleCard handleSubmit={handleSubmit} />
-        </div>
-        {/* Dividing Line */}
-        <div className="border-b border-gray-300 w-full"></div>
-
-        {/* Playlist Card */}
-        {playlists && (
-          <div className="bg-white">
-            <PlaylistReference playlists={playlists} isPlaylistsLoading={isPlaylistsLoading} />
+        {/* Grid State A: Just the Title/Prompt card */}
+        {/* {!playlists && 
+          <div className="grid grid-cols-1 grid-rows-1 gap-4 p-8">
+            <TitleCard handleSubmit={handleSubmit} />
           </div>
-        )}
-        {/* Dividing Line */}
-        <div className="border-b border-gray-300 w-full"></div>
+        } */}
 
-        {/* Response Card */}
-        {promptResponse && (
-          <div className="bg-red-100">
-            <Response promptResponse={promptResponse} isPromptLoading={isPromptLoading} recv={recv} />
+        {/* Grid State C: 2 rows, 3 columns */}
+          <div className="grid grid-cols-3 grid-rows-3 gap-4 p-8">
+            {/* Title/Prompt Card */}
+            <div className="col-start-1 col-end-3 row-span-1 bg-red-200">
+              <TitleCard handleSubmit={handleSubmit} />
+            </div>
+
+            {/* Playlist Reference Card */}
+            <div className="col-start-3 col-end-3 row-span-2 bg-white">
+              <PlaylistReference playlists={playlists} isPlaylistsLoading={isPlaylistsLoading} />
+            </div>
+
+            {/* Response Card */}
+            <div className="col-start-1 col-end-3 row-span-2 bg-red-100">
+              <Response promptResponse={promptResponse} isPromptLoading={isPromptLoading} recv={recv} />
+            </div>
           </div>
-        )}
+
+        {/* Test Controls */}
+        {/* <div className="mb-4">
+          <button
+            onClick={scrapePlaylists}
+            className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md mr-4"
+          >
+            {isPlaylistsLoading ? "Loading Playlists..." : "Load Playlists"}
+          </button>
+          <button
+            onClick={sendPrompt}
+            className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md mr-4"
+          >
+            {isPromptLoading ? "Loading Response..." : "Load Response"}
+          </button>
+          <button
+            onClick={handleLoadAll}
+            className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md"
+          >
+            Load All
+          </button>
+        </div> */}
       </div>
     </div>
   );
